4c34c527214bc678a64f8e875e5bffa77f17864f
Crizant Lai <crizant3908@gmail.com>
2021-08-16 16:48:18 +0100

    add pull to refresh

--- lib/table_body.dart
+++ lib/table_body.dart
@@ -37,45 +37,59 @@ class _TableBodyState extends State<TableBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          width: cellWidth,
-          child: ListView(
-            controller: _firstColumnController,
-            physics: ClampingScrollPhysics(),
-            children: List.generate(maxNumber - 1, (index) {
-              return MultiplicationTableCell(
-                color: Colors.yellow.withOpacity(0.3),
-                value: index + 2,
-              );
-            }),
+    return RefreshIndicator(
+      notificationPredicate: (ScrollNotification notification) {
+        return notification.depth == 0 || notification.depth == 1;
+      },
+      onRefresh: () async {
+        await Future.delayed(
+          Duration(seconds: 2),
+        );
+      },
+      child: Row(
+        children: [
+          SizedBox(
+            width: cellWidth,
+            child: ListView(
+              controller: _firstColumnController,
+              physics: const AlwaysScrollableScrollPhysics(
+                parent: BouncingScrollPhysics(),
+              ),
+              children: List.generate(maxNumber - 1, (index) {
+                return MultiplicationTableCell(
+                  color: Colors.yellow.withOpacity(0.3),
+                  value: index + 2,
+                );
+              }),
+            ),
           ),
-        ),
-        Expanded(
-          child: SingleChildScrollView(
-            controller: widget.scrollController,
-            scrollDirection: Axis.horizontal,
-            physics: const ClampingScrollPhysics(),
-            child: SizedBox(
-              width: (maxNumber - 1) * cellWidth,
-              child: ListView(
-                controller: _restColumnsController,
-                physics: const ClampingScrollPhysics(),
-                children: List.generate(maxNumber - 1, (y) {
-                  return Row(
-                    children: List.generate(maxNumber - 1, (x) {
-                      return MultiplicationTableCell(
-                        value: (x + 2) * (y + 2),
-                      );
-                    }),
-                  );
-                }),
+          Expanded(
+            child: SingleChildScrollView(
+              controller: widget.scrollController,
+              scrollDirection: Axis.horizontal,
+              physics: const ClampingScrollPhysics(),
+              child: SizedBox(
+                width: (maxNumber - 1) * cellWidth,
+                child: ListView(
+                  controller: _restColumnsController,
+                  physics: const AlwaysScrollableScrollPhysics(
+                    parent: BouncingScrollPhysics(),
+                  ),
+                  children: List.generate(maxNumber - 1, (y) {
+                    return Row(
+                      children: List.generate(maxNumber - 1, (x) {
+                        return MultiplicationTableCell(
+                          value: (x + 2) * (y + 2),
+                        );
+                      }),
+                    );
+                  }),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
